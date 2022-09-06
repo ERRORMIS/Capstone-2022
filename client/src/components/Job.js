72f9169d5d4b5ea65cd774819bba565e7bb4a81d
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2022-09-06 16:57:49 +0530

    Fixed some bugs in All Projects page

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -5,7 +5,6 @@ import { useAppContext } from '../context/appContext'
 import Wrapper from '../assets/wrappers/Job'
 import JobInfo from './JobInfo'
 
-//All Projects pages
 const Job = ({
   _id,
   title,
